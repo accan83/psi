05a034b42682def29dd21f1e39be6a7522a46f02
M Hasan <accan83@gmail.com>
2016-05-02 14:39:23 +0700

    Create ReservedMaterial Model

---
+++ backend/models/ReservedMaterial.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "reserved_material".
+ *
+ * @property integer $id
+ * @property integer $created_at
+ *
+ * @property ReservedMaterialDetail[] $reservedMaterialDetails
+ * @property Material[] $materials
+ */
+class ReservedMaterial extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'reserved_material';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['created_at'], 'required'],
+            [['created_at'], 'integer']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'created_at' => 'Created At',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getReservedMaterialDetails()
+    {
+        return $this->hasMany(ReservedMaterialDetail::className(), ['reserved_material_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getMaterials()
+    {
+        return $this->hasMany(Material::className(), ['id' => 'material_id'])->viaTable('reserved_material_detail', ['reserved_material_id' => 'id']);
+    }
+}
