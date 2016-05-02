956a354e8addc383422a5f5c7de8178449ab59cf
M Hasan <accan83@gmail.com>
2016-05-02 14:40:02 +0700

    Create Product Model

---
+++ backend/models/Product.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "product".
+ *
+ * @property integer $order_id
+ * @property integer $material_id
+ * @property string $name
+ * @property integer $qty
+ *
+ * @property Material $material
+ * @property Order $order
+ */
+class Product extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'product';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['order_id', 'material_id', 'name', 'qty'], 'required'],
+            [['order_id', 'material_id', 'qty'], 'integer'],
+            [['name'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'order_id' => 'Order ID',
+            'material_id' => 'Material ID',
+            'name' => 'Name',
+            'qty' => 'Qty',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getMaterial()
+    {
+        return $this->hasOne(Material::className(), ['id' => 'material_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getOrder()
+    {
+        return $this->hasOne(Order::className(), ['id' => 'order_id']);
+    }
+}
