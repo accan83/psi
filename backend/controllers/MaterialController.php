fa92d83ed79f0b89d2b7f50529000493e30d6298
M Hasan <accan83@gmail.com>
2016-05-09 01:29:03 +0700

    kontrol master material

---
+++ backend/controllers/MaterialController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace backend\controllers;
+
+use Yii;
+use common\models\Material;
+use common\models\Stock;
+use yii\data\ActiveDataProvider;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * MaterialController implements the CRUD actions for Material model.
+ */
+class MaterialController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Material models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $dataProvider = new ActiveDataProvider([
+            'query' => Material::find(),
+        ]);
+
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Material model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new Material model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Material();
+        $model2 = new Stock();
+
+        if ($model->load(Yii::$app->request->post()) && $model2->load(Yii::$app->request->post())) {
+            if ($model->save()) {
+                $model2->material_id = $model->id;
+                if ($model2->save()) {
+                    return $this->redirect(['view', 'id' => $model->id]);
+                }
+            }
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+                'model2' => $model2,
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing Material model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+        $model2 = $model->stock;
+
+        if ($model->load(Yii::$app->request->post()) && $model2->load(Yii::$app->request->post())) {
+            if ($model->save() && $model2->save()) {
+                return $this->redirect(['view', 'id' => $model->id]);
+            }
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+                'model2' => $model2,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Material model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->stock->delete();
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Material model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Material the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Material::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
