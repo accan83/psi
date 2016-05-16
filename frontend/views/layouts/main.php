c51335550c068ffa2067ed532b1716d541ec9054
M Hasan <accan83@gmail.com>
2016-05-16 17:43:00 +0700

    tambah menu Request

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -37,6 +37,7 @@ AppAsset::register($this);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
         ['label' => 'Order', 'url' => ['order/index']],
+        ['label' => 'Request', 'url' => ['request/index']],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
