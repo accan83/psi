ec184f2f6ed95579db086c09e9e4b14eb675ad22
M Hasan <accan83@gmail.com>
2016-05-16 11:24:16 +0700

    tambah menu order

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -36,8 +36,7 @@ AppAsset::register($this);
     ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
+        ['label' => 'Order', 'url' => ['order/index']],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
