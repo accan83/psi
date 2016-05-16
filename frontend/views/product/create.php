bc271f5b9e6b8c1bfcaecdf4b59c95a45e435fca
M Hasan <accan83@gmail.com>
2016-05-16 11:26:00 +0700

    ubah Create Product ke Add Product

--- frontend/views/product/create.php
+++ frontend/views/product/create.php
@@ -6,13 +6,15 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model backend\models\Product */
 
-$this->title = 'Create Product';
-$this->params['breadcrumbs'][] = ['label' => 'Products', 'url' => ['index']];
+$order_id = Yii::$app->request->get('order_id');
+$this->title = 'Add Product';
+$this->params['breadcrumbs'][] = ['label' => 'Orders', 'url' => ['order/index']];
+$this->params['breadcrumbs'][] = ['label' => '#' . $order_id, 'url' => ['order/view', 'id' => $order_id]];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="product-create">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1><?= Html::encode('Order #' . $order_id . ': ' . $this->title) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
