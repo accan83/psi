44a78758e371903dbefee6aa68f73fad6331030d
M Hasan <accan83@gmail.com>
2016-06-25 10:30:00 +0700

    Create Page

--- frontend/views/accepted/_form.php
+++ frontend/views/accepted/_form.php
@@ -12,9 +12,7 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'reserved_material_id')->textInput() ?>
-
-    <?= $form->field($model, 'created_at')->textInput() ?>
+    <?= $form->field($model, 'reserved_material_id')->hiddenInput()->label('') ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
--- frontend/views/accepted/create.php
+++ frontend/views/accepted/create.php
@@ -1,20 +1,40 @@
 <?php
 
+use common\models\ReservedMaterialDetail;
+use yii\data\ActiveDataProvider;
+use yii\grid\GridView;
 use yii\helpers\Html;
 
 
 /* @var $this yii\web\View */
 /* @var $model common\models\MaterialAccepted */
 
-$this->title = 'Create Material Accepted';
-$this->params['breadcrumbs'][] = ['label' => 'Material Accepteds', 'url' => ['index']];
+$this->title = 'Create Accepted';
+$this->params['breadcrumbs'][] = ['label' => 'Materials Accepted', 'url' => ['index']];
+$this->params['breadcrumbs'][] = 'Reserved ' . date('d M Y', $reserved->created_at);
 $this->params['breadcrumbs'][] = $this->title;
+$model->reserved_material_id = $reserved->id;
+
+$dataProvider = new ActiveDataProvider([
+    'query' => ReservedMaterialDetail::find()->where(['reserved_material_id' => $reserved->id]),
+]);
 ?>
 <div class="material-accepted-create">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'material.code',
+            'qty',
+        ],
+    ]); ?>
+
     <?= $this->render('_form', [
+        'dataProvider' => $dataProvider,
         'model' => $model,
     ]) ?>
 
