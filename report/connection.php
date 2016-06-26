14ad7ce7d28e82a59ece4401c4906979f3fedc7d
M Hasan <accan83@gmail.com>
2016-06-27 00:44:37 +0700

    user-pass DB @ server

--- report/connection.php
+++ report/connection.php
@@ -1,7 +1,9 @@
 <?php
 $server = "localhost"; 
-$username = "itcc";
-$password = "itcc2015";
+//$username = "itcc";
+//$password = "itcc2015";
+$username = "psi";
+$password = "P51Y11Du4";
 $database = "psi";
 
 
