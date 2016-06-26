d91f66a00426ca32fd57c658ad8db956342d9431
M Hasan <accan83@gmail.com>
2016-06-26 23:55:05 +0700

    percantik tampilan iframe

--- backend/views/site/report.php
+++ backend/views/site/report.php
@@ -5,5 +5,16 @@
 $this->title = 'My Yii Application';
 ?>
 <div class="site-report">
-    <iframe width="100%" height="500" src="http://report.psi.m-hasan.my.id"></iframe>
+    <script type="text/javascript">
+        function iframeLoaded() {
+            var iFrameID = document.getElementById('idIframe');
+            if(iFrameID) {
+                // here you can make the height, I delete it first, then I make it again
+                iFrameID.height = "";
+                iFrameID.height = iFrameID.contentWindow.document.body.scrollHeight + "px";
+            }
+        }
+    </script>
+
+    <iframe id="idIframe" onload="iframeLoaded()" width="100%" scrolling="no" frameborder="0" src="http://report.psi.m-hasan.my.id"></iframe>
 </div>
