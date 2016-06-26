502dad97c59526f415ffed1ee204a060ae237f19
M Hasan <accan83@gmail.com>
2016-06-27 01:55:13 +0700

    fixing

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'Sakura Painting';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Wellcome to Sakura Painting Application!</h1>
+        <h1>Wellcome!</h1>
 
         <p class="lead">You have successfully login as <?= Yii::$app->user->identity->username ?> .</p>
     </div>
