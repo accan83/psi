5ee34327378d0523df9eeab8fb254ae92fa65032
M Hasan <accan83@gmail.com>
2016-06-27 00:36:19 +0700

    percantik dengan loading

--- backend/views/site/report.php
+++ backend/views/site/report.php
@@ -6,12 +6,14 @@ $this->title = 'My Yii Application';
 ?>
 <div class="site-report">
 
-    <iframe id="idIframe" width="100%" scrolling="no" frameborder="0" src="http://report.psi.m-hasan.my.id"></iframe>
+    <h3 class="text-center" id="loading">Loading Page...</h3>
+    <iframe id="idIframe" width="100%" scrolling="no" frameborder="0" src="http://report.psi.m-hasan.my.id">Loading</iframe>
     <script type="text/javascript">
         (function() {
             // your page initialization code here
             // the DOM will be available here
             var iFrameID = document.getElementById('idIframe');
+            var loading = document.getElementById('loading');
             if(iFrameID) {
                 iFrameID.height = "0";
             }
@@ -22,8 +24,8 @@ $this->title = 'My Yii Application';
                 if (~event.origin.indexOf('http://report.psi.m-hasan.my.id')) {
                     // The data has been sent from your site
                     if(iFrameID) {
-                        iFrameID.height = "";
                         iFrameID.height = event.data + "px";
+                        loading.style.display = 'none';
                     }
                 } else {
                     // The data hasn't been sent from your site!
